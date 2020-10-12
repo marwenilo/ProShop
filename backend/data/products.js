e30991d2421295a285ac7cafc09a45b2974baf14
marwen <marwenmagrii@gmail.com>
2020-10-12 17:42:17 +0100

    ES modules change

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,5 +1,3 @@
-const { builtinModules } = require("module")
-
 const products = [
   {
     _id: '1',
@@ -81,4 +79,4 @@ const products = [
   },
 ]
 
-module.exports =  products
+export default products
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
+dotenv.config()
 const app = express()
 
 app.get('/', (req, res) => {
@@ -12,4 +15,9 @@ app.get('/api/products/:id', (req, res) => {
   const product = products.find((item) => item._id === req.params.id)
   res.json(product)
 })
-app.listen(5000, console.log('Server Running on port 5000'))
+
+const PORT = process.env.PORT || 5000
+app.listen(
+  PORT,
+  console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
