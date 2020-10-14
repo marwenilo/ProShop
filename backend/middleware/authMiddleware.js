d80e31bf7eae8319371804ee9cab050381cd89ae
marwen <marwenmagrii@gmail.com>
2020-10-14 16:16:52 +0100

    log and reg user

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -6,7 +6,7 @@ const protect = asyncHandler(async (req, res, next) => {
   let token
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith('bearer')
+    req.headers.authorization.startsWith('Bearer')
   ) {
     try {
       token = req.headers.authorization.split(' ')[1]
